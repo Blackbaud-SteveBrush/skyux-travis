69b490b202c152617760705b5807eaf0461c51e8
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-06 11:55:15 -0400

    Sourcing nvm

--- v3/library-before-install.sh
+++ v3/library-before-install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+# Make nvm available in this script.
+. ~/.nvm/nvm.sh
+
 # Necessary to stop pull requests from forks from running.
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  npm install --latest-npm
+  nvm install-latest-npm
   npm install -g @blackbaud/skyux-cli
 else
   echo -e "Ignoring script. Pull requests from forks are run elsewhere."
