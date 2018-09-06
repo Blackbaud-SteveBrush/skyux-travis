3c8dc0e63968134a39390efd12c4aa53126f830e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-06 12:06:16 -0400

    Added skyux cli version log

--- v3/library-before-install.sh
+++ v3/library-before-install.sh
@@ -8,6 +8,7 @@ set -e
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   nvm install-latest-npm
   npm install -g @blackbaud/skyux-cli
+  skyux --version
 else
   echo -e "Ignoring script. Pull requests from forks are run elsewhere."
 fi
