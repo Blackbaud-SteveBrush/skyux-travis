32fd2c4c639967b10e9671254ed59ab3814c5252
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-05-26 15:19:44 -0400

    Try catching errors

--- v3/library-after-success.sh
+++ v3/library-after-success.sh
@@ -9,26 +9,33 @@ if [[ "$TRAVIS_SECURE_ENV_VARS" == "true" ]]; then
   # Save any new baseline screenshots.
   if [ -d "./node_modules/@skyux-sdk/builder" ]; then
     echo -e "Running visual baseline scripts from @skyux-sdk/builder..."
-    output=$(node ./node_modules/@skyux-sdk/builder-config/scripts/visual-baselines.js) || exit
+    # output=$(node ./node_modules/@skyux-sdk/builder-config/scripts/visual-baselines.js) || exit
+
+    node "./node_modules/@skyux-sdk/builder-config/scripts/visual-baselines.js" "$input"
+    echo "Script: $? - Successfull"
+    if [ $? != 0 ]; then
+      echo "${?}\n". 1>&2 && exit 1
+    fi
+
   else
     echo -e "Running visual baseline scripts from @blackbaud/skyux-builder-config..."
-    output=$(node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js) || exit
+    # output=$(node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js) || exit
   fi
 
   # Only run releases during a git tag build.
   if [[ -n "$TRAVIS_TAG" ]]; then
 
-    # Allow package.json to specify a custom build script.
-    if npm run | grep -q build:ci; then
-      echo -e "Running custom build step... `npm run build:ci`";
-      npm run build:ci
-    else
-      echo -e "Building the library via `skyux build-public-library`..."
-      skyux build-public-library
-    fi
+    # # Allow package.json to specify a custom build script.
+    # if npm run | grep -q build:ci; then
+    #   echo -e "Running custom build step... `npm run build:ci`";
+    #   npm run build:ci
+    # else
+    #   echo -e "Building the library via `skyux build-public-library`..."
+    #   skyux build-public-library
+    # fi
 
-    cd dist
-    bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
+    # cd dist
+    # bash <(curl -s https://blackbaud.github.io/skyux-travis/after-success.sh)
   else
     echo -e "Aborting release. Releases are published after git tag builds only."
   fi
